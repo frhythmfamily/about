e769b7d00e4860d377ab73b02372242182b24b1a
frhythmfamily <frhythmfamily@gmail.com>
2024-01-16 16:57:34 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// Toogle class active hamburger menu
+const navbarNav = document.querySelector('.navbar-nav');
+const navbar = document.querySelector('.navbar');
+
+let isOpen = false;
+// ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').onclick = (e) => {
+    if (isOpen) {
+        navbarNav.classList.toggle('active');
+        setTimeout(() => {
+            navbar.style.overflow = 'hidden';
+        }, 100);
+        isOpen = false;
+        e.preventDefault();
+    } else {
+        navbarNav.classList.toggle('active');
+        navbar.style.overflow = 'visible';
+        isOpen = true;
+        e.preventDefault();
+    }
+};
+
+// klik di luar element untuk menghilangkan nav
+const hm = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function (e) {
+    if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+        setTimeout(() => {
+            navbar.style.overflow = 'hidden';
+        }, 100);
+        isOpen = false;
+    }
+});
+
+// Parallax
+const image = document.getElementById('img1');
+const text = document.getElementById('text1');
+
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+    let widthPercentage = Math.min(value, 100);
+    image.style.width = widthPercentage + '%';
+});
